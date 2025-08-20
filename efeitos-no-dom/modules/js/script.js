4183350fd22464ae500eac1c7eb37b693b3ec152
matheustorresdev97 <matheustorresdev@gmail.com>
2025-08-20 18:40:45 -0300

    efeitos no dom

---
+++ efeitos-no-dom/modules/js/script.js
@@ -0,0 +1,16 @@
+import { initAccordion } from "./modules/accordion.js";
+import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
+import { initScrollSuave } from "./modules/scroll-suave.js";
+import { initTabNav } from "./modules/tabnav.js";
+import * as teste from "./modules/teste.js";
+
+
+initAccordion();
+initAnimacaoScroll();
+initScrollSuave();
+initTabNav();
+
+console.log(teste);
+teste.teste1();
+teste.teste2();
+console.log(teste.senha);
